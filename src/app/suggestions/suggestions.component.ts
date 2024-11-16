04a63cecd8f9653b6fd15900c430a7b61a54a3e6
Jeth0214 <rolandjethrosuyom@yahoo.com>
2024-11-16 14:33:40 +0300

    working on layout

--- src/app/suggestions/suggestions.component.ts
+++ src/app/suggestions/suggestions.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faPlus } from '@fortawesome/free-solid-svg-icons'
 
 @Component({
   selector: 'app-suggestions',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './suggestions.component.html',
   styleUrl: './suggestions.component.scss'
 })
 export class SuggestionsComponent {
+  plusIcon = faPlus;
 
 }
--- src/app/toolbar/toolbar.component.ts
+++ src/app/toolbar/toolbar.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { DropdownComponent } from '../shared/components/dropdown/dropdown.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faPlus } from '@fortawesome/free-solid-svg-icons'
 
 @Component({
   selector: 'app-toolbar',
   standalone: true,
-  imports: [],
+  imports: [DropdownComponent, FontAwesomeModule],
   templateUrl: './toolbar.component.html',
-  styleUrl: './toolbar.component.scss'
+  styleUrl: './toolbar.component.scss',
 })
 export class ToolbarComponent {
+  title: string = 'Suggestions';
+  suggestionsCount: number = 6;
 
+  faPlus = faPlus;
 }
