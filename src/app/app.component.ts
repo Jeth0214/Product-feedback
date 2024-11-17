4d565279fc7d7c0f0b79c04b2ce45f050222404a
Jeth0214 <rolandjethrosuyom@yahoo.com>
2024-11-17 18:24:35 +0300

    worked on responsive layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ToolbarComponent } from './toolbar/toolbar.component';
-import { SuggestionsComponent } from './suggestions/suggestions.component';
-import { CategoryComponent } from "./category/category.component";
-import { RoadmapComponent } from './roadmap/roadmap.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    RouterOutlet,
-    ToolbarComponent,
-    SuggestionsComponent,
-    RoadmapComponent,
-    CategoryComponent
-],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
