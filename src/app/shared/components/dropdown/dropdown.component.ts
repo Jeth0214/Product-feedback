4e58ba97a9ec89f753223a4eb5cd6aeeb54f4c5e
Jeth0214 <rolandjethrosuyom@yahoo.com>
2024-12-03 19:16:13 +0300

    working on dropdown filter

--- src/app/shared/components/dropdown/dropdown.component.ts
+++ src/app/shared/components/dropdown/dropdown.component.ts
@@ -9,4 +9,10 @@ import { Component } from '@angular/core';
 })
 export class DropdownComponent {
 
+
+  isDropdownOpen = false;
+
+  toggleDropdown() {
+    this.isDropdownOpen = !this.isDropdownOpen;
+  }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [ "./src/**/*.{html,ts}",],
+  content: [
+    "./src/**/*.{html,ts}",
+        "./node_modules/flowbite/**/*.js"
+  ],
   theme: {
     extend: {
       colors: {
@@ -35,6 +38,8 @@ module.exports = {
       }
     },
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ],
 }
 
