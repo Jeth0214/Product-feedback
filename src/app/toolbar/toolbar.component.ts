14036f64955f7438afe02b444637ef0e4d15fce6
Jeth0214 <rolandjethrosuyom@yahoo.com>
2024-12-15 04:16:15 +0300

    style raodmap card depending on status

--- src/app/toolbar/toolbar.component.ts
+++ src/app/toolbar/toolbar.component.ts
@@ -1,17 +1,20 @@
-import { Component } from '@angular/core';
+import { Component , Input} from '@angular/core';
 import { DropdownComponent } from '../shared/components/dropdown/dropdown.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faPlus } from '@fortawesome/free-solid-svg-icons'
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-toolbar',
   standalone: true,
-  imports: [DropdownComponent, FontAwesomeModule],
+  imports: [DropdownComponent, FontAwesomeModule, RouterLink],
   templateUrl: './toolbar.component.html',
   styleUrl: './toolbar.component.scss',
 })
 export class ToolbarComponent {
-  title: string = 'Suggestions';
+
+  @Input() title: string = '';
+  @Input() isSuggestionPage: boolean = true;
   suggestionsCount: number = 6;
 
   faPlus = faPlus;
