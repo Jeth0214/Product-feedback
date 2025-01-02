8fbd82ab5f6cb6339600f3006fe875260a126035
Jeth0214 <rolandjethrosuyom@yahoo.com>
2025-01-02 19:10:29 +0300

    worked on adding comment and reply

--- src/app/feedbacks/feedback-details/feedback-comments/feedback-comments.component.ts
+++ src/app/feedbacks/feedback-details/feedback-comments/feedback-comments.component.ts
@@ -1,28 +1,28 @@
 import { Component, computed,  input } from '@angular/core';
 import { IComment } from '../../../shared/models/comment.model';
-import { IReply } from '../../../shared/models/replies.model';
-import { NgClass } from '@angular/common';
+import { NgClass, NgIf } from '@angular/common';
 import { FeedbackCommentFormComponent } from '../feedback-comment-form/feedback-comment-form.component';
 
 @Component({
   selector: 'app-feedback-comments',
-  imports: [NgClass, FeedbackCommentFormComponent],
+  imports: [NgClass, FeedbackCommentFormComponent, NgIf],
   templateUrl: './feedback-comments.component.html',
   styleUrl: './feedback-comments.component.scss'
 })
 export class FeedbackCommentsComponent {
 
 
-  comment = input.required<IComment | IReply>();
-  commentStatus = input.required<'firstComment' | 'reply'>();
+  comment = input.required<Partial<IComment>>();
   isLast = input<boolean>();
-  replyBorder = computed(() => this.commentStatus() == 'reply' ? ' border-l  md:ms-6 ps-6' : '');
+  replyBorder = computed(() => this.comment().replyingTo ? ' border-l  md:ms-6 ps-6' : '');
 
   showCommentForm =  false;
 
 
   onReply() {
     this.showCommentForm = !this.showCommentForm;
-   }
+  }
+  
+  
 
 }
