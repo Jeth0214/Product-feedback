40348cf0296ddd03decc5a61a494344d919deb77
Jeth0214 <rolandjethrosuyom@yahoo.com>
2025-01-03 18:47:01 +0300

    worked on adding comments through API

--- src/app/shared/services/feedbacks.service.ts
+++ src/app/shared/services/feedbacks.service.ts
@@ -1,9 +1,10 @@
-import { inject, Injectable, signal } from "@angular/core";
+import { computed, inject, Injectable, signal } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
-import { catchError, firstValueFrom, Observable, of } from 'rxjs';
+import { catchError, firstValueFrom, Observable, of, tap } from 'rxjs';
 import { IFeedBack } from "../models/feedbacks.model";
 import { ToastrService } from "ngx-toastr";
 import { Router } from "@angular/router";
+import { IComment } from "../models/comment.model";
 
 @Injectable({
   providedIn: 'root',
@@ -14,20 +15,47 @@ export class FeedBackService {
 
   //properties
   api: string = 'api/feedBacks';
+
+  jsonAPI: string = 'https://jsonplaceholder.typicode.com/posts';
+
+  CAAPI = 'http://localhost:8000/api/departments'
  
   // innjections
   _http = inject(HttpClient);
   _toastrService = inject(ToastrService);
   _router = inject(Router);
 
+
+  async addPOst(): Promise<any> {
+    let data = {
+      name: 'Test Department',  
+    };
+    const response$ = await this._http.post(this.CAAPI, data).pipe(
+      tap((response) => console.log('Post response', response)),
+      catchError(
+        this.handleError<any>('Add Post', {})
+    ));
+    const newPost = await firstValueFrom(response$);
+    return newPost;
+    
+  }
+
+  addPostObservable(): Observable<any> {
+      let data = {
+      name: 'Test Department',  
+    };
+    return  this._http.post(this.CAAPI, data).pipe(
+      tap((response) => console.log('Post response', response)),
+      catchError(
+        this.handleError<any>('Add Post', {})
+    ));
+   }
+
   // signals
   feedBacks = signal<IFeedBack[]>([])
 
-  getFeedBacks(): Observable<IFeedBack[]> {
-    return this._http.get<IFeedBack[]>(this.api);
-  }
 // Fetches all feedbacks from the API
- async getAllFeedBacks(): Promise<IFeedBack[]>{ 
+ async getAllFeedBacks(): Promise<IFeedBack[]> { 
     const response$ = await this._http.get<IFeedBack[]>(this.api).pipe(
       catchError(
         this.handleError<IFeedBack[]>('Get All Feedbacks', [] as IFeedBack[])
@@ -55,15 +83,21 @@ async addFeedback(feedback: Partial<IFeedBack>): Promise<IFeedBack> {
   const newFeedback = await firstValueFrom(response$);
   this.feedBacks.update((current) => [...current, newFeedback]);
   return newFeedback;
-}
+  }
+  
+  addFeedBackObservable(feedback: Partial<IFeedBack>): Observable<IFeedBack> { 
+    return this._http.post<IFeedBack>(this.api, feedback);
+  }
 
 // Updates an existing feedback in the API and updates the feedbacks signal
 async updateFeedback(id: number, feedback: IFeedBack): Promise<IFeedBack> {
-  const response$ = await this._http.put<IFeedBack>(`${this.api}/${id}`, feedback).pipe(
+  const response$ = await this._http.put<IFeedBack>(this.api, feedback).pipe(
+    tap((response) => console.log('Feedback updated response', response)),
     catchError(
       this.handleError<IFeedBack>('Update Feedback', {} as IFeedBack)
   ));
   const updatedFeedback = await firstValueFrom(response$);
+  console.log('Updated from service', updatedFeedback);
   this.feedBacks.update((current) => current.map(fb => fb.id === id ? updatedFeedback : fb));
   return updatedFeedback;
 }
@@ -76,7 +110,29 @@ async deleteFeedback(id: number): Promise<void> {
   ));
   await firstValueFrom(response$);
   this.feedBacks.update((current) => current.filter(fb => fb.id !== id));
-}
+  }
+  
+
+  // add comment to feedback in the API and updates the feedbacks signal
+  async addComment(feedbackId: number, commentData: Partial<IComment>): Promise<void> {
+
+    const feedback = computed(() => this.feedBacks().filter(fb => fb.id === feedbackId));
+    console.log('feedback', feedback());
+
+    // const response$ = await this._http.post<IComment>(`${this.api}/${feedbackId}/comments`, commentData).pipe(
+    //   catchError(
+    //     this.handleError<IComment>('Add Comment', {} as IComment)
+    // ));
+    // const newComment = await firstValueFrom(response$);
+
+    // console.log('newComment', newComment);
+    //     this.feedBacks.update((current) => current.map(fb => {
+    //     if (fb.id === feedbackId) {
+    //         const comments = fb.comments ?? [];
+    //         return { ...fb, comments: [...comments, newComment] };
+    //     }
+    // }));
+   }
 
   // error handling 
  private handleError<T>(operation = 'operation', result?: T) {
