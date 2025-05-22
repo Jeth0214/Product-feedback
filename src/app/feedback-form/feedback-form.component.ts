0aa7aabe1eee892ec60d765077d1eb024793fc6c
Jeth0214 <rolandjethrosuyom@yahoo.com>
2025-05-22 07:12:20 +0300

    worked on feedback add form

--- src/app/feedback-form/feedback-form.component.ts
+++ src/app/feedback-form/feedback-form.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { DropdownComponent } from '../shared/components/dropdown/dropdown.component';
 
 @Component({
   selector: 'app-feedback-form',
-  imports: [RouterLink],
+  imports: [RouterLink, DropdownComponent],
   templateUrl: './feedback-form.component.html',
   styleUrl: './feedback-form.component.scss'
 })
@@ -11,5 +12,7 @@ export class FeedbackFormComponent {
 
   formIcon = 'assets/shared/icon-new-feedback.svg';
   title = 'Create New Feedback';
+
+  categories = ['Feature', 'UI', 'UX', 'Enhancement', 'Bug'];
 
 }
