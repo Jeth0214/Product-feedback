415ce4bdbca4b079d0436498cdbed93162d6a52e
Jeth0214 <rolandjethrosuyom@yahoo.com>
2025-06-22 06:44:42 +0300

    add card detail link on Roadmap card

--- src/app/roadmap/roadmap-list-card/roadmap-list-card.component.ts
+++ src/app/roadmap/roadmap-list-card/roadmap-list-card.component.ts
@@ -4,10 +4,11 @@ import { faChevronUp, faCircle } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { NgClass } from '@angular/common';
 import { UpvoteButtonComponent } from '../../shared/components/upvote-button/upvote-button.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
     selector: 'app-roadmap-list-card',
-    imports: [FontAwesomeModule, NgClass, UpvoteButtonComponent],
+    imports: [FontAwesomeModule, NgClass, UpvoteButtonComponent, RouterLink],
     templateUrl: './roadmap-list-card.component.html',
     styleUrl: './roadmap-list-card.component.scss'
 })
