e71306fd270cefe4d4c7b60407dafe8ce65e0f2f
Jeth0214 <rolandjethrosuyom@yahoo.com>
2025-07-19 07:34:09 +0300

    Refactored feedback service

--- src/app/shared/functions/countComment.ts
+++ src/app/shared/functions/countComment.ts
@@ -1,8 +1,6 @@
 import { IComment } from "../models/comment.model";
 
 export function countComment(comments: IComment[] | undefined): number {
-  if (!comments) {
-    return 0;
-  }
-  return comments.reduce((count, comment) => count + 1 + (comment.replies?.length || 0), 0);
+
+  return comments?.reduce((count, comment) => count + 1 + (comment.replies?.length || 0), 0) || 0;
 }
--- src/app/shared/services/feedbacks.service.ts
+++ src/app/shared/services/feedbacks.service.ts
@@ -1,13 +1,10 @@
-import { computed, DestroyRef, effect, inject, Injectable, OnDestroy, signal, WritableSignal } from "@angular/core";
+import { computed, DestroyRef, inject, Injectable, signal } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
-import { catchError, finalize, firstValueFrom, Observable, of, Subject, tap } from 'rxjs';
-import { takeUntil } from "rxjs/operators";
+import { finalize, Observable, tap } from 'rxjs';
 import { IFeedBack } from "../models/feedbacks.model";
 import { ToastrService } from "ngx-toastr";
 import { Router } from "@angular/router";
-import { IComment } from "../models/comment.model";
-import { takeUntilDestroyed, toSignal  } from "@angular/core/rxjs-interop";
-import { LoadingService } from "./loading.service";
+import { takeUntilDestroyed } from "@angular/core/rxjs-interop";
 
 
 @Injectable({
@@ -17,197 +14,137 @@ import { LoadingService } from "./loading.service";
 
 export class FeedBackService  {
 
-  //properties
-  private api: string = 'api/feedBacks';
+  //core
+  private readonly api: string = 'api/feedBacks';
   private destroy$ = inject(DestroyRef)
  
-  // innjections
-  private _http = inject(HttpClient);
-  private _toastrService = inject(ToastrService);
-  private _router = inject(Router);
-  private _loadingService = inject(LoadingService);
-  
+  // Dependencies
+  private readonly http = inject(HttpClient);
+  private readonly toastr = inject(ToastrService);
+  private readonly router = inject(Router);
 
 
   // signals
-  private feedBacks = signal<IFeedBack[]>([]);
-  selectedFeedBack = signal<IFeedBack>({} as IFeedBack); 
-  categoryTerm = signal('All');
-  sortValue = signal('Most Upvotes');
-  isLoading = signal(false);
+  private readonly feedBacks = signal<IFeedBack[]>([]);
+  readonly selectedFeedBack = signal<IFeedBack>({} as IFeedBack); 
+  readonly categoryTerm = signal('All');
+  readonly sortValue = signal('Most Upvotes');
+  
+  // loading signals
+  readonly isFetchingFeedBacks = signal(false);
+  readonly isFetchingSelectedFeedBack = signal(false);
+  readonly isUpVotingFeedback = signal(false);
+  readonly isUpdatingFeedback = signal(false);
+  readonly isCommenting = signal(false);
 
 
-  filteredFeedBacks = computed(() => {
-    const category = this.categoryTerm();
-    const feedBacks = this.feedBacks();
-    const sortValue = this.sortValue();
-
-    // First, filter the feedbacks by category
-    let filtered = this.filterFeedBacksByCategory(category, feedBacks);
-
-    // Then, sort the filtered feedbacks based on the selected sort value
-    let sorted = this.sortFeedBacksByValue(sortValue, filtered);
-
-    return sorted;
+  readonly filteredFeedBacks = computed(() => {
+    const filtered = this.categoryTerm() === 'All'
+      ? this.feedBacks()
+      : this.feedBacks().filter(fb => 
+        fb.category.toLowerCase() === this.categoryTerm().toLowerCase()
+      )
+    
+    return this.sortFeedBacks( this.sortValue(), filtered)
   })
 
 
-  
-  // Fetches all feedbacks from the API and sets the feedbacks signal
+  // Public Methods
   getAllFeedBacks() { 
-    this._loadingService.loadingOn();
-    this._http.get<IFeedBack[]>(this.api)
+    this.isFetchingFeedBacks.set(true);
+    this.http.get<IFeedBack[]>(this.api)
       .pipe(
         tap((response) => this.feedBacks.set(response)),
-        catchError( this.handleError<IFeedBack[]>('Get All Feedbacks', [] as IFeedBack[])),
-        takeUntilDestroyed(this.destroy$) // Clean up subscription when the service is destroyed
-      ).subscribe(response => { 
-        this._loadingService.loadingOff();
-      });
+        finalize(() => this.isFetchingFeedBacks.set(false)),
+        takeUntilDestroyed(this.destroy$)
+      ).subscribe();
     
   }
 
 // Fetches a specific feedback by its ID from the API
   getFeedBackById(id: number) {
-    this._loadingService.loadingOn();
-    this._http.get<IFeedBack>(`${this.api}/${id}`).pipe(
-      catchError( 
-        this.handleError<IFeedBack>('Get Feedback By id', {} as IFeedBack)
-      ),
-      takeUntilDestroyed(this.destroy$) // Clean up subscription when the service is destroyed
-    ).subscribe((response) => {
-      this._loadingService.loadingOff();
-      this.selectedFeedBack.set(response)
-    } );
+    this.isFetchingSelectedFeedBack.set(true);
+    this.http.get<IFeedBack>(`${this.api}/${id}`).pipe(
+      tap(( response ) => this.selectedFeedBack.set(response)),
+      finalize(() => this.isFetchingSelectedFeedBack.set(false)),
+      takeUntilDestroyed(this.destroy$) 
+    ).subscribe();
   }
 
-  // Adds a new feedback to the API and updates the feedbacks signal
-  // addFeedBack(feedback: Partial<IFeedBack>) {
-  //   this.isLoading.set(true);
-  //   this._http.post<IFeedBack>(this.api + 's', feedback)
-  //     .pipe(
-  //       catchError(
-  //         this.handleError<IFeedBack>('Add Feedback', {} as IFeedBack)
-  //       ),
-  //       finalize( () => this.isLoading.set(false)),
-  //       takeUntilDestroyed(this.destroy$) // Clean up subscription when the service is destroyed
-  //     )
-  //     .subscribe((newFeedback) => {
-  //       this.feedBacks.update((current) => [...current, newFeedback]);
-  //       this._toastrService.success('Feedback added successfully', 'Success');
-  //       this._router.navigate(['/feedbacks']);
-  //      })
-  // }
 
-  // Adds a new feedback to the API using Obervable
+
+
   addFeedBack(feedback: Partial<IFeedBack>): Observable<IFeedBack> {
-   return this._http.post<IFeedBack>(this.api, feedback)
+   return this.http.post<IFeedBack>(this.api, feedback)
   }
 
   
-
-  // Updates an existing feedback in the API and updates the feedbacks signal
-  // !! NOTE : angular-in-memory-web-api PUT method did not return the updated object, 
-  // !! so we are returning the same object that was sent to the API if successful
-  // updateFeedBack(feedback: Partial<IFeedBack>) {
-  //     this.isLoading.set(true);
-  //   this._http.put<IFeedBack>(this.api, feedback).pipe(
-  //     catchError(this.handleError<IFeedBack>('Update Feedback', {} as IFeedBack)),
-  //     finalize(() => this.isLoading.set(false)),
-  //   ).subscribe(() => {
-  //     this._toastrService.success(`Feedback with ${feedback.id} updated successfully`, 'Success');
-  //   });
-  // }
   
   updateFeedBack(feedback: Partial<IFeedBack>): Observable<IFeedBack> {
-    return this._http.put<IFeedBack>(this.api, feedback).pipe(
-      tap(() => {this.selectedFeedBack.set({...this.selectedFeedBack(), ...feedback})}),
-      catchError(this.handleError<IFeedBack>('Update Feedback', {} as IFeedBack))
+    return this.http.put<IFeedBack>(this.api, feedback).pipe(
+      tap(() => {this.selectedFeedBack.set({...this.selectedFeedBack(), ...feedback})})
     );
   }
 
-    // !! NOTE : angular-in-memory-web-api PUT method did not return the updated object, 
-  // !! so we are returning the same object that was sent to the API if successful
   upVoteFeedBack(upvotedFeedback: IFeedBack) {
-    this._http.put<IFeedBack>(this.api, upvotedFeedback).pipe(
-      catchError(this.handleError<IFeedBack>('Update Feedback', {} as IFeedBack)),
+    this.http.put<IFeedBack>(this.api, upvotedFeedback).pipe(
+      tap(() => {
+        this.feedBacks.update(list =>
+            list.map(fb => (fb.id === upvotedFeedback.id ? upvotedFeedback : fb))
+          );
+          this.selectedFeedBack.set(upvotedFeedback);
+          this.toastr.success('Feedback upvoted successfully', 'Success');
+      }),
       takeUntilDestroyed(this.destroy$)
-    ).subscribe(() => {
-      this.feedBacks.update( (feedBacks: IFeedBack[]) => {
-        // search the feedback on feedbacks and replace it
-        return  feedBacks.map( feedBack => upvotedFeedback.id === feedBack.id ? {...feedBack, ...upvotedFeedback} : feedBack)
-        
-      })
-      this.selectedFeedBack.set(upvotedFeedback);
-      this._toastrService.success(`Feedback upvoted successfully`, 'Success');
-    });
+    ).subscribe();
   }
   
 
 
-// Deletes a feedback from the API and updates the feedbacks signal
+  // Deletes a feedback from the API and updates the feedbacks signal
   deleteFeedBack(id: number): Observable<IFeedBack> {
-    return this._http.delete<IFeedBack>(`${this.api}/${id}`)
+    return this.http.delete<IFeedBack>(`${this.api}/${id}`)
   }
   
 
-
-  // error handling 
- private handleError<T>(operation = 'operation', result?: T) {
-  return (error: any): Observable<T> => {
-    // show error alert message
-    this._toastrService.error(`${operation} failed: ${error.body.error}`,  `Status: ${error.status}`);
-    // Let the app keep running by returning an empty result.
-    return of(result as T);
-  };
-  }
   
-
+  
   // Filter Setter
   setCategoryTerm(category: string) {
     this.categoryTerm.set(category);
   }
+  
+
 
   // Sort Setter
   setSortValue(value: string) {
     this.sortValue.set(value);
   }
 
-  // get all comments lenght for a specific feedback
-  private getCommentsLength(feedBack: IFeedBack): number {
-    if (!feedBack.comments) return 0;
-    return feedBack.comments.reduce((count, comment) => {
-      const replies = comment.replies ? comment.replies.length : 0;
-      return count + 1 + replies;
-     },0);
+
+  // Private methods
+  private sortFeedBacks(type: string, list: IFeedBack[]): IFeedBack[] {
+    const getCommentCount = (fb: IFeedBack) =>
+      fb.comments?.reduce((acc, c) => acc + 1 + (c.replies?.length || 0), 0) || 0;
+
+      switch (type) {
+        case 'Most Upvotes':
+          return [...list].sort((a, b) => b.upvotes - a.upvotes);
+        case 'Least Upvotes':
+          return [...list].sort((a, b) => a.upvotes - b.upvotes);
+        case 'Most Comments':
+          return [...list].sort((a, b) => getCommentCount(b) - getCommentCount(a));
+        case 'Least Comments':
+          return [...list].sort((a, b) => getCommentCount(a) - getCommentCount(b));
+        default:
+            
+          return list;
+      }
   }
 
-  // filter feedbacks by category
-  private filterFeedBacksByCategory(category: string, feedBacks: IFeedBack[]): IFeedBack[] {
-    if (category === 'All') {
-      return feedBacks;
-    } else {
-      return feedBacks.filter((feedback: IFeedBack) => feedback.category.toLowerCase() === category.toLowerCase());
-    }
-  }
-
-  // sort feedbacks by sort value
-  private sortFeedBacksByValue(sortValue: string, feedBacks: IFeedBack[]): IFeedBack[] {
-    switch (sortValue) {
-      case 'Most Upvotes':
-        return feedBacks.sort((a, b) => b.upvotes - a.upvotes);
-      case 'Least Upvotes':
-        return feedBacks.sort((a, b) => a.upvotes - b.upvotes);
-      case 'Most Comments':
-        return feedBacks.sort((a, b) => this.getCommentsLength(b) - this.getCommentsLength(a));
-      case 'Least Comments':
-        return feedBacks.sort((a, b) => this.getCommentsLength(a) - this.getCommentsLength(b));
-      default:
-        return feedBacks;
-    }
-  }
 
   
 }
+
 
 
